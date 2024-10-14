d5cab04c84c6c03b010613b843c57b2db2e9a150
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2024-10-14 16:39:08 +0200

    Commit 1: Rough design created

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputForm = document.querySelector('input-form'); 
+
+const binBtn = document.querySelectorAll('bin-icon');
+
+inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const newTask = document.querySelector('input-field');
+})
